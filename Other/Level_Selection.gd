d764315e2b1533887c9c686d80c3b3cf64e43623
RJPrinceNZ <rjprincenz@gmail.com>
2021-06-15 14:58:12 +1200

    Level1and2done

---
+++ Other/Level_Selection.gd
@@ -0,0 +1,28 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Other/Level_1.tscn")
+
+
+func _on_Back_pressed():
+	get_tree().change_scene("res://Other/Title.tscn")
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://Other/Level_2.tscn")
